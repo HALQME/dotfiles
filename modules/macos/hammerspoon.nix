cbd39a4e3b4a7332e72838af2739dfddb8c4232e
HAL <68320771+HALQME@users.noreply.github.com>
2026-03-01 16:23:12 +0900

    Remove Hammerspoon and Karabiner macOS modules

--- modules/macos/hammerspoon.nix
+++
@@ -1,7 +0,0 @@
-{ pkgs, lib, ... }:
-let
-  isDarwin = builtins.hasAttr "stdenv" pkgs && (pkgs.stdenv.isDarwin or false);
-in
-lib.mkIf isDarwin {
-  home.file.".hammerspoon".source = ./../../config/hammerspoon;
-}
--- modules/macos/karabiner.nix
+++
@@ -1,7 +0,0 @@
-{ pkgs, lib, ... }:
-let
-  isDarwin = builtins.hasAttr "stdenv" pkgs && (pkgs.stdenv.isDarwin or false);
-in
-lib.mkIf isDarwin {
-  home.file.".config/karabiner".source = ./../../config/karabiner;
-}
