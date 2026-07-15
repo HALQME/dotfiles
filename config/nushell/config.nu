c89bf478c1c223c1a7ed8c548d07039ee8940162
HAL <68320771+HALQME@users.noreply.github.com>
2026-07-15 18:02:34 +0900

    Load vite-plus env and tune Codex config

--- config/nushell/config.nu
+++ config/nushell/config.nu
@@ -4,3 +4,5 @@ $env.config.show_banner = false
 $env.config.buffer_editor = "nvim"
 $env.EDITOR = "nvim"
 $env.VISUAL = "nvim"
+
+source '~/.vite-plus/env.nu'
--- config/zsh/darwin/10-paths.zsh
+++ config/zsh/darwin/10-paths.zsh
@@ -1,7 +1,6 @@
 export SSH_AUTH_SOCK="$HOME/Library/Containers/com.maxgoedjen.Secretive.SecretAgent/Data/socket.ssh"
 
 path=(
-  "/opt/homebrew/bin"
   "$HOME/.local/bin"
   "$HOME/.modular/pkg/packages.modular.com_mojo/bin"
   "$HOME/.ghcup/bin"
